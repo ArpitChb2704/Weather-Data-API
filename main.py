b973dcd46962781374a710a0041f22e73c084513
Arpit Chhabra <156332722+ArpitChb2704@users.noreply.github.com>
2024-06-13 18:00:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+import pandas as pd
+# create the Flask app
+
+app = Flask(__name__)
+
+stations = pd.read_csv("app6_weather_api/data_small/stations.txt",skiprows=17)
+stations=stations[["STAID","STANAME                                 "]]
+
+
+@app.route('/')
+def home():
+    return render_template('home.html',data=stations.to_html())
+
+@app.route('/api/v1/<station>/<date>')
+def about(station, date):
+    filename = "app6_weather_api/data_small/TG_STAID"+str(station).zfill(6)+".txt"
+    df = pd.read_csv(filename,skiprows=20,parse_dates=["    DATE"])
+    temperature = df.loc[df["    DATE"]==date]["   TG"].squeeze()/10
+    return {"station":station, "date":date, "temperature":temperature}
+
+@app.route('/api/v1/<station>')
+def all_data(station):
+    filename = "app6_weather_api/data_small/TG_STAID"+str(station).zfill(6)+".txt"
+    df = pd.read_csv(filename,skiprows=20,parse_dates=["    DATE"])
+    result = df.to_dict(orient='records')
+    return result
+
+@app.route('/api/v1/yearly/<station>/<year>')
+def year_data(station, year):
+    filename = "app6_weather_api/data_small/TG_STAID"+str(station).zfill(6)+".txt"
+    df = pd.read_csv(filename,skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df.loc[df["    DATE"].str.startswith(year)].to_dict(orient='records')
+    return result
+    
+    
+    
+
+
+if __name__ == '__main__':  
+    app.run(debug=True)
